fe711109ad04c91c91b8a6359d6bc0680cc8459c
Ben Shabowski <benshabowski@gmail.com>
2022-10-13 18:58:46 -0500

    Initial Commit

---
+++ Deep Rock Compansion/Deep_Rock_CompansionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deep_Rock_CompansionApp.swift
+//  Deep Rock Compansion
+//
+//  Created by Benjamin Shabowski on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Deep_Rock_CompansionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
